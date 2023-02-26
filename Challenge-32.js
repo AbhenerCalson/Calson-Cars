9cbaf87576db6d34dce2c7c62dc774c23306f326
Abhener Calson <108624737+AbhenerCalson@users.noreply.github.com>
2023-02-26 18:29:04 -0300

    Add files via upload

--- Challenge-32.js
+++ Challenge-32.js
@@ -1,124 +1,27 @@
-(function( DOM ) {
-    'use strict';
-  
-   
-    var app = (function appController() {
-      return {
-        init: function init() {
-          console.log( 'app init' );
-          this.companyInfo();
-          this.initEvents();
-          this.getCars();
-        },
-
-        initEvents: function initEvents() {
-          new DOM( '[data-js="form-register"]' ).on( 'submit', this.handleSubmit );     
-        },
-
-        handleSubmit: function handleSubmit( e ) {
-          e.preventDefault();
-          var $tableCar = new DOM( '[data-js="table-car"]' ).get();
-
-          var carImage = new DOM('[data-js="image"]').get();
-          var brandModel = new DOM('[data-js="brand-model"]').get();
-          var year = new DOM('[data-js="year"]').get();
-          var plate = new DOM('[data-js="plate"]').get();
-          var color = new DOM('[data-js="color"]').get();
-
-          var postCar = new XMLHttpRequest();
-          postCar.open( 'POST', 'http://localhost:3000/car' );
-          postCar.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
-          postCar.send(`image=${carImage.value}
-                        &brandModel=${brandModel.value}
-                        &year=${year.value}
-                        &plate=${plate.value}
-                        &color=${color.value}`);
-          postCar.onreadystatechange = function() {
-            if( this.readyState === 4 ) {
-            app.getCars();
-            }
-          } 
-          $tableCar.appendChild( app.createNewCar() ) 
-        },
-
-        createNewCar: function createNewCar() {
-          var $fragment = document.createDocumentFragment();
-          var $tr = document.createElement('tr');
-          var $tdImage = document.createElement('td');
-          var $tdBrand = document.createElement('td');
-          var $tdYear = document.createElement('td');
-          var $tdPlate = document.createElement('td');
-          var $tdColor = document.createElement('td');
-          var $tdRemove = document.createElement('td');
-          var $buttonRemove = document.createElement('button')
-          var $image = document.createElement('img');
-  
-          $tr.setAttribute('id', new DOM('[data-js="plate"]').get().value);
-  
-          $image.setAttribute('src', new DOM('[data-js="image"]').get().value);
-          $tdImage.appendChild($image)
-  
-          $tdRemove.appendChild($buttonRemove);
-          $buttonRemove.innerHTML = 'Remover';
-          $buttonRemove.setAttribute('data-remove', new DOM('[data-js="plate"]').get().value);
-          $buttonRemove.classList.add('btn-remove');
-  
-          $buttonRemove.addEventListener('click', function(e) {
-            e.preventDefault();
-            $tr.parentNode.removeChild($tr); });
-          
-          $tdBrand.textContent = new DOM('[data-js="brand-model"]').get().value;
-          $tdYear.textContent = new DOM('[data-js="year"]').get().value;
-          $tdPlate.setAttribute('class', 'plate');
-          $tdPlate.textContent = new DOM('[data-js="plate"]').get().value;
-          $tdColor.textContent = new DOM('[data-js="color"]').get().value;
-  
-          $tr.appendChild($tdImage);
-          $tr.appendChild($tdBrand);
-          $tr.appendChild($tdYear);
-          $tr.appendChild($tdPlate);
-          $tr.appendChild($tdColor);
-          $tr.appendChild($tdRemove);
-  
-          return $fragment.appendChild($tr);
-        },
-
-        getCars: function getCars() {
-          var getCars = new XMLHttpRequest();
-          getCars.open( 'GET', 'http://localhost:3000/car' );
-          getCars.send();
-
-          getCars.onreadystatechange = function() {
-            if(!(app.isReady.call(this))) {            
-              return;
-            }
-          }
-        },
-
-        companyInfo: function companyInfo() {
-          var ajax = new XMLHttpRequest();
-          ajax.open( 'GET', 'company.json', true );
-          ajax.send();
-          ajax.addEventListener( 'readystatechange', this.getCompanyInfo, false );
-        },
-
-        getCompanyInfo: function getCompanyInfo() {
-          if ( !app.isReady.call( this ) )
-            return
-            var companyData = JSON.parse(this.responseText);
-            var $companyName = new DOM( '[data-js="company-name"]' ).get();
-            var $companyPhone = new DOM( '[data-js="company-phone"]' ).get();
-            $companyName.textContent = companyData.name;
-            $companyPhone.textContent = companyData.phone;
-        },
-
-        isReady: function isReady() {
-          return this.readyState === 4 && this.status === 200
-        }
-
-      };
-    })();
-
-    app.init();
-
-  })( window.DOM );
+/*
+Já temos as funcionalidades de adicionar e remover um carro. Agora, vamos persistir esses dados, 
+salvando-os temporariamente na memória de um servidor.
+Nesse diretório do `challenge-32` tem uma pasta `server`. É um servidor simples, em NodeJS, para 
+que possamos utilizar para salvar as informações dos nossos carros.
+Para utilizá-lo, você vai precisar fazer o seguinte:
+- Via terminal, acesse o diretório `server`;
+- execute o comando `npm install` para instalar as dependências;
+- execute `node app.js` para iniciar o servidor.
+Ele irá ser executado na porta 3000, que pode ser acessada via browser no endereço: 
+`http://localhost:3000`
+O seu projeto não precisa estar rodando junto com o servidor. Ele pode estar em outra porta.
+As mudanças que você irá precisar fazer no seu projeto são:
+- Para listar os carros cadastrados ao carregar o seu projeto, faça um request GET no endereço
+`http://localhost:3000/car`
+- Para cadastrar um novo carro, faça um POST no endereço `http://localhost:3000/car`, enviando
+os seguintes campos:
+  - `image` com a URL da imagem do carro;
+  - `brandModel`, com a marca e modelo do carro;
+  - `year`, com o ano do carro;
+  - `plate`, com a placa do carro;
+  - `color`, com a cor do carro.
+Após enviar o POST, faça um GET no `server` e atualize a tabela para mostrar o novo carro cadastrado.
+Crie uma branch `challenge-32` no seu projeto, envie um pull request lá e cole nesse arquivo a URL
+do pull request.
+*/
+console.log('Link do pull request do seu projeto');
